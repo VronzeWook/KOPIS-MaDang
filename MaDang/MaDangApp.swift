863c9f40291e3770dd30d3e79596732278fe8b30
VronzeWook <chanel0222@naver.com>
2024-08-15 17:30:08 +0900

    init

---
+++ MaDang/MaDangApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaDangApp.swift
+//  MaDang
+//
+//  Created by LDW on 8/15/24.
+//
+
+import SwiftUI
+
+@main
+struct MaDangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
