1037e0f78a3c40c8a4ba3cb05acfc138776244fb
VronzeWook <chanel0222@naver.com>
2024-08-16 19:44:26 +0900

    [Setting] #4 - Color 추가

---
+++ MaDang/MaDang/Helper/Color+.swift
@@ -0,0 +1,19 @@
+//
+//  Color+.swift
+//  MaDang
+//
+//  Created by LDW on 8/16/24.
+//
+
+import SwiftUI
+
+extension UIColor {
+    /// Primary ColorSet
+    ///
+    /// NineYellow color - (250, 255, 0, 100%)
+    class var NineYellow: UIColor? { return UIColor(named: "NineYellow") }
+    /// NineDarkGray color - (51, 51, 51, 100%)
+    class var NineDarkGray: UIColor? { return UIColor(named: "NineDarkGray") }
+    /// Tertiary color - (0, 0, 0, 100%)
+    class var NineBlack: UIColor? { return UIColor(named: "NineBlack") }
+}
