fc355128c9dcc5bfb9ca1f47100519bdcf31cfd3
VronzeWook <chanel0222@naver.com>
2024-08-16 15:41:50 +0900

    Add new Xcode project

---
+++ MaDang/MaDang/MaDangApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaDangApp.swift
+//  MaDang
+//
+//  Created by LDW on 8/16/24.
+//
+
+import SwiftUI
+
+@main
+struct MaDangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
