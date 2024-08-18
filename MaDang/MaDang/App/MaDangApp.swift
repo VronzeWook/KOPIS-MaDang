fa3b1aca40412914a0372db5a171b0ed29bfd05d
VronzeWook <chanel0222@naver.com>
2024-08-19 04:05:14 +0900

    [Design] #22 - 잔업 추가

--- MaDang/MaDang/App/MaDangApp.swift
+++ MaDang/MaDang/App/MaDangApp.swift
@@ -9,14 +9,9 @@ import SwiftUI
 
 @main
 struct MaDangApp: App {
-    init() {
-        DataManager.shared.fetchData()
-    }
-    
     var body: some Scene {
         WindowGroup {
-            //ContentView()
-            MainView()
+            MainTabView()
         }
     }
 }
