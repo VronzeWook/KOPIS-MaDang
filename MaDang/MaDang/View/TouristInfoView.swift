0d8c5d391ed21277aaa8661a109443fb7ba8177d
VronzeWook <chanel0222@naver.com>
2024-08-20 01:59:28 +0900

    [Design] #33 - TouristInfoView 파일 생성

---
+++ MaDang/MaDang/View/TouristInfoView.swift
@@ -0,0 +1,18 @@
+//
+//  TouristInfoView.swift
+//  MaDang
+//
+//  Created by LDW on 8/20/24.
+//
+
+import SwiftUI
+
+struct TouristInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TouristInfoView()
+}
